2e3ac702599c145e280342fc2decf5664a28979f
Hariri A. <110659046+haririabd@users.noreply.github.com>
2025-08-16 17:52:42 +0000

    adding function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,22 @@
+import type { MouseEvent } from "react";
+
 function ListGroup() {
-  const items = [
-    'New York',
-    'SanFrancisco',
-    'Tokyo',
-    'London',
-    'Paris'
-];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event)
+
   return (
     // Empty angle bracket is telling react to use fragments to the children
     <>
       <h1>List</h1>
+      {/* If the item length is 0, show the statement. Otherwise, the statemnt is false so nothing will be rendered */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {/* below is how we loop the data above to be rendered in li */}
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>{item}</li>
+        ))}
       </ul>
     </>
   );
