b4735b1457a7de87369d3875492a49990adde8b3
Hariri A. <110659046+haririabd@users.noreply.github.com>
2025-08-16 21:51:22 +0000

    added state and props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,39 @@
-import type { MouseEvent } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
 
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event)
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     // Empty angle bracket is telling react to use fragments to the children
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/* If the item length is 0, show the statement. Otherwise, the statemnt is false so nothing will be rendered */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {/* below is how we loop the data above to be rendered in li */}
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>{item}</li>
+          <li
+            className={
+              selectedIndex == index // if this is true, then:
+                ? "list-group-item active" // if true
+                : "list-group-item" // if false
+            }
+            key={item}
+            onClick={()  => { 
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
